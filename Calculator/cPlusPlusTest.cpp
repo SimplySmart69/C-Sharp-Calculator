d8da6685714c1ebb1f205b67447fc9b07e7c3e69
SimplySmart69 <83801170+SimplySmart69@users.noreply.github.com>
2021-06-08 13:21:27 -0500

    Add files via upload

---
+++ Calculator/cPlusPlusTest.cpp
@@ -0,0 +1,14 @@
+// my first program in C++
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+   string name;
+ 
+   cout << "Please enter your name: ";
+   cin >> name;
+   cout << "Your name is: " << name << endl;
+ 
+}
